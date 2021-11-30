8967de7a90ec6cbaef87180024754612b2cb01d7
Yury Prokhorov <5071177@gmail.com>
2021-11-30 10:47:03 +0300

    Initial Commit

---
+++ RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by Yury Prokhorov on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
